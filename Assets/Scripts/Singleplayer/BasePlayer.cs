46ab03150c89691673cc5fc3b8dee5186c387126
RoccoMichel <122682577+RoccoMichel@users.noreply.github.com>
2025-01-16 20:55:58 +0100

    Base Player

---
+++ Assets/Scripts/Singleplayer/BasePlayer.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BasePlayer : MonoBehaviour
+{
+    [Header("Properties")]
+    public float moveSpeed;
+    public float jumpStrength;
+    public float gravity;
+    public bool grounded;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    [Header("References")]
+    public CharacterController controller;
+    public Transform groundCheck;
+
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    void Update()
+    {
+        // Movement
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        Vector3 move = transform.right * moveValue.x + transform.forward * moveValue.y;
+
+        if (grounded && velocity.y < 0) velocity.y = -2f;
+        velocity.y += gravity * Time.deltaTime;
+
+        if (jumpAction.IsPressed() && grounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpStrength * -2 * gravity);
+        }
+
+        controller.Move(velocity * Time.deltaTime);
+        controller.Move(moveSpeed * Time.deltaTime * move);
+
+    }
+}
